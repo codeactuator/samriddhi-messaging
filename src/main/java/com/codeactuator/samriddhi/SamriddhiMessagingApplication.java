97f13d9cbcce25b1a0a56a6a8b3c516dea4d22f1
Shekhar Kumar <shekharkumargupta@gmail.com>
2020-09-06 10:01:51 +0530

    initial commit

---
+++ src/main/java/com/codeactuator/samriddhi/SamriddhiMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.codeactuator.samriddhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamriddhiMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamriddhiMessagingApplication.class, args);
+	}
+
+}
